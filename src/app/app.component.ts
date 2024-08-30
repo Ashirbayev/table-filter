8925090243fb46ca65cdb5e95e302377d0a476d9
Yerlan <E.Ashirbayev@gmail.com>
2024-08-30 12:12:08 +0500

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import data from '../assets/data';
+
+interface User {
+  _id: string;
+  isActive: boolean;
+  balance?: string;
+  picture: string;
+  age: number;
+  name: {
+    first: string;
+    last: string;
+  };
+  company: string;
+  email: string;
+  address: string;
+  tags: string[];
+  favoriteFruit: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'test-table';
+export class AppComponent implements OnInit {
+  initialDisplayedColumns: string[] = ['id', 'name', 'age', 'company', 'email', 'balance'];
+  columnDefinitions = [
+    { def: 'id', header: 'ID', visible: true },
+    { def: 'name', header: 'Name', visible: true },
+    { def: 'age', header: 'Age', visible: true },
+    { def: 'company', header: 'Company', visible: true },
+    { def: 'email', header: 'Email', visible: true },
+    { def: 'balance', header: 'Balance', visible: true }
+  ];
+  dataSource = new MatTableDataSource<User>([]);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.dataSource.data = data as User[];
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    }, Math.random() * 2000);
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  get displayedColumns() {
+    return this.columnDefinitions.filter(cd => cd.visible).map(cd => cd.def);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatTableModule } from '@angular/material/table';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSortModule} from '@angular/material/sort';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -10,7 +19,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
